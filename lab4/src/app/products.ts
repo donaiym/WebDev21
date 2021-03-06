4741ce77876bb16b36e41fd63dd1e41c22e357e3
donaiym <48382437+donaiym@users.noreply.github.com>
2021-03-06 15:49:54 +0600

    Create products.ts

---
+++ lab4/src/app/products.ts
@@ -0,0 +1,122 @@
+export const products = [
+  {
+    id: 1,
+    name: 'NVIDIA Jetson Xavier NX Developer Kit',
+    price: '399$',
+    description: 'The NVIDIA Jetson Xavier NX developer kit includes a power-efficient, compact Jetson Xavier NX module for AI edge devices. It benefits from new cloud-native support and accelerates the NVIDIA software stack in as little as 10 W with more than 10X the performance of its widely adopted predecessor, Jetson TX2. The capability to develop and test power-efficient, small form-factor solutions with accurate, multi-modal AI inference opens the door for new breakthrough products.',
+    rating: 5,
+    image1: 'https://images-na.ssl-images-amazon.com/images/I/71TiOw6fSuL._AC_SL1500_.jpg',
+    image2: 'https://images-na.ssl-images-amazon.com/images/I/71VojyweOjL._AC_SL1500_.jpg',
+    image3: 'https://images-na.ssl-images-amazon.com/images/I/71wCqr4Jd-L._AC_SL1500_.jpg',
+    link: 'https://www.amazon.com/NVIDIA-Jetson-Xavier-Developer-812674024318/dp/B086874Q5R/ref=sr_1_5?dchild=1&fst=as%3Aoff&pd_rd_r=bed16e3c-a82f-422d-b221-b851e9c17987&pd_rd_w=oNxS7&pd_rd_wg=pe0nA&pf_rd_p=83ab1c34-7657-4d09-b72d-0a3e4b634b1d&pf_rd_r=7WTETX9XCCV1CHDCAEEN&qid=1614507382&rnid=16225007011&s=computers-intl-ship&sr=1-5'
+  },
+  {
+    id: 2,
+    name: 'Libre Computer Board AML-S905X-CC (Le Potato) 2GB 64-bit Mini Computer for 4K Media',
+    price: '35$',
+    description: 'Key Features:\n' +
+      '\n' +
+      'Quad-Core 64-bit ARMv8 Cortex-A53 CPU with NEON and Crypto Extensions\n' +
+      'Penta-Core ARM Mali-450 MP3 GPU with OpenGL ES 2.0 @ 750MHz\n' +
+      '2GB of DDR3-2133 SDRAM on 32-bit Bus for up to 69Gb/s Unified Memory Bandwidth\n' +
+      'Multi-Plane Multi-Format 2.5D Programmable Pipeline Display Engine with HDMI 2.0 Output\n' +
+      '4K Ultra-HD High Dynamic Range Video Engine with Hardware Accelerated Decoder for VP9, H.265, H.264\n' +
+      'Form Factor and GPIO Compatibility with Raspberry Pi 3 for Maximum Re-usability',
+    rating: 4,
+    image1: 'https://images-na.ssl-images-amazon.com/images/I/91iQjFVDjcL._AC_SL1500_.jpg',
+    image2: 'https://images-na.ssl-images-amazon.com/images/I/81cHSFz1K8L._AC_SL1500_.jpg',
+    image3: 'https://images-na.ssl-images-amazon.com/images/I/810UzZPo3kL._AC_SL1500_.jpg',
+    link: 'https://www.amazon.com/Libre-Computer-AML-S905X-CC-Potato-64-bit/dp/B074P6BNGZ/ref=sr_1_14_sspa?dchild=1&fst=as%3Aoff&pd_rd_r=bed16e3c-a82f-422d-b221-b851e9c17987&pd_rd_w=oNxS7&pd_rd_wg=pe0nA&pf_rd_p=83ab1c34-7657-4d09-b72d-0a3e4b634b1d&pf_rd_r=7WTETX9XCCV1CHDCAEEN&qid=1614507382&rnid=16225007011&s=computers-intl-ship&sr=1-14-spons&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUExOElDQTUzN1hPTUtaJmVuY3J5cHRlZElkPUEwNjIzMzc3MUdPWTlLRVEyUkhSNSZlbmNyeXB0ZWRBZElkPUEwNzkzMjgxMjZEVlNCMlJBMTBRSSZ3aWRnZXROYW1lPXNwX210Zl9icm93c2UmYWN0aW9uPWNsaWNrUmVkaXJlY3QmZG9Ob3RMb2dDbGljaz10cnVl'
+  },
+  {
+    id: 3,
+    name: 'ISEEVY WiFi H.264 HDMI Video Encoder Wireless IPTV',
+    price: '175$',
+    description: 'H.264 WiFi HDMI Video Encoder is a professional audio & video encoding device. From any HDMI input source, it generates H.264/MPEG-4 video encoding and AAC/MP3 audio code 4 streams compliant with RTMP/RTSP/UDP/HTTP/HLS/FLV/TS protocols.',
+    rating: 3.5,
+    image1: 'https://images-na.ssl-images-amazon.com/images/I/51OnIAbh0uL._AC_SL1000_.jpg',
+    image2: 'https://images-na.ssl-images-amazon.com/images/I/51O5V-qfwcL._AC_SL1000_.jpg',
+    image3: 'https://images-na.ssl-images-amazon.com/images/I/51DosmSPbKL._AC_SL1000_.jpg',
+    link: 'https://www.amazon.com/Wirelees-Broadcast-Protocols-Facebook-Platforms/dp/B077NF1PSP/ref=sr_1_6?dchild=1&keywords=wifi+cracker&qid=1614507767&sr=8-6'
+  },
+  {
+    id: 4,
+    name: 'J-Tech Digital H.264 IP Encoder',
+    price: '155.95$',
+    description: 'J-Tech Digital manufactures a variety of electronic and audio-video devices for multiple professional markets and industries. J-Tech Digital upholds the highest business standards and practices that follow and meet both domestic and international compliance, such as FCC, CCC, CE, RoHS, KCC, SAA, RCM, and REACH.',
+    rating: 4.5,
+    image1: 'https://images-na.ssl-images-amazon.com/images/I/71WXV5ptISL._AC_SL1500_.jpg',
+    image2: 'https://images-na.ssl-images-amazon.com/images/I/71mrEn0-fxL._AC_SL1500_.jpg',
+    image3: 'https://images-na.ssl-images-amazon.com/images/I/71I0JD5wDnL._AC_SL1500_.jpg',
+    link: 'https://www.amazon.com/J-Tech-Digital-Supports-Protocol-Broadcast/dp/B0761X6L3C/ref=pd_sbs_2?pd_rd_w=RDzXR&pf_rd_p=1f09e623-97c8-4c74-883f-063b7295f49e&pf_rd_r=Y01TPCV66FV52WVFJFD3&pd_rd_r=25c8dfcb-7a66-4675-92d3-aa70f6301b70&pd_rd_wg=6ivb1&pd_rd_i=B0761X6L3C&psc=1'
+  },
+  {
+    id: 5,
+    name: 'ISEEVY Mini HD 1080P HDMI Video Encoder H.264 IPTV',
+    price: '155$',
+    description: 'H.264 Mini HDMI Video Encoder IPTV Encoder RTMP Live Encoder is a professional HD audio & video encoding device. From any HDMI input source, it generates H.264/MPEG-4 video encoding and AAC/MP3 audio code 4 streams compliant with RTMP/RTSP/UDP/HTTP/HLS/FLV/TS protocols.',
+    rating: 5,
+    image1: 'https://images-na.ssl-images-amazon.com/images/I/61AHnSCa80L._AC_SL1000_.jpg',
+    image2: 'https://images-na.ssl-images-amazon.com/images/I/61qWvN6ZdpL._AC_SL1000_.jpg',
+    image3: 'https://images-na.ssl-images-amazon.com/images/I/61CsaB6b5aL._AC_SL1000_.jpg',
+    link: 'https://www.amazon.com/dp/B074H1X7QJ/ref=sspa_dk_detail_1?psc=1&pd_rd_i=B074H1X7QJ&pd_rd_w=tYfK0&pf_rd_p=4269e1a0-a218-4fbd-9748-1cd337d2f2a5&pd_rd_wg=6ivb1&pf_rd_r=Y01TPCV66FV52WVFJFD3&pd_rd_r=25c8dfcb-7a66-4675-92d3-aa70f6301b70&smid=A35UF28265N4PU&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUEzUDZENzAwTFhMTTNVJmVuY3J5cHRlZElkPUEwOTkwNTA2VTRSUlFaVlpBOFRaJmVuY3J5cHRlZEFkSWQ9QTAxNTk5MDYyQlA3NDlIVUkySFE2JndpZGdldE5hbWU9c3BfZGV0YWlsJmFjdGlvbj1jbGlja1JlZGlyZWN0JmRvTm90TG9nQ2xpY2s9dHJ1ZQ=='
+  },
+  {
+    id: 6,
+    name: 'FMUSER FBE200 H.264/H.265 HDMI Encoder Video Encoder IPTV',
+    price: '158$',
+    description: 'VIDEO Encoding:\n' +
+      'Video Compression: H.264/H.265 / AVC\n' +
+      'Video Inputs: 1 x HDMI 1.3a version (support HDCP protocol)',
+    rating: 4,
+    image1: 'https://images-na.ssl-images-amazon.com/images/I/61L94a0FRhL._AC_SL1001_.jpg',
+    image2:'https://images-na.ssl-images-amazon.com/images/I/61Y3qrIyT0L._AC_SL1001_.jpg',
+    image3: 'https://images-na.ssl-images-amazon.com/images/I/61ZJrUZqRAL._AC_SL1001_.jpg',
+    link: 'https://www.amazon.com/Encoder-Broadcast-Support-Multicast-Unicast/dp/B01MU2WVO7/ref=pd_di_sccai_3?pd_rd_w=jSNer&pf_rd_p=c9443270-b914-4430-a90b-72e3e7e784e0&pf_rd_r=THR8W3AC7V86NFG7PRAW&pd_rd_r=6179e8d8-ec4d-4bdc-a84c-c6f5f5c820ac&pd_rd_wg=LISvO&pd_rd_i=B01MU2WVO7&psc=1'
+  },
+  {
+    id: 7,
+    name: 'DDMALL Mini 4K HDMI Encoder',
+    price: '699$',
+    description: 'The DDMALL HEV-4K encoder features real 4K encoding, and has both the newer H.265 codec as well as the H.264 codec in the same small unit. It encodes audio/video from HDMI source and delivers Ultra HD quality streams up to 3820×2160@30Hz or Full HD streams at 1080p with low encoding latency over wired Ethernet.',
+    rating: 3,
+    image1: 'https://images-na.ssl-images-amazon.com/images/I/51%2B8zJSkZAL._AC_SL1000_.jpg',
+    image2: 'https://images-na.ssl-images-amazon.com/images/I/51MQ96vjYLL._AC_SL1000_.jpg',
+    image3: 'https://images-na.ssl-images-amazon.com/images/I/616rQ5xbCIL._AC_SL1000_.jpg',
+    link: 'https://www.amazon.com/dp/B07X8N1BC5/ref=sspa_dk_detail_2?psc=1&pd_rd_i=B07X8N1BC5&pd_rd_w=fF44J&pf_rd_p=4269e1a0-a218-4fbd-9748-1cd337d2f2a5&pd_rd_wg=5aVEd&pf_rd_r=V2D3RZHENQXBS10KST15&pd_rd_r=468878e7-a28a-4881-b078-b89f8aed62bc&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUExODlaTVdIVUo3TVlJJmVuY3J5cHRlZElkPUEwOTE1NjE4MVhKMzFZVjdTMVBYQyZlbmNyeXB0ZWRBZElkPUEwNzU3NzAyMU9LQU84TDY5UVIzUiZ3aWRnZXROYW1lPXNwX2RldGFpbCZhY3Rpb249Y2xpY2tSZWRpcmVjdCZkb05vdExvZ0NsaWNrPXRydWU='
+  },
+  {
+    id: 8,
+    name: 'Blackmagic Design UltraStudio Mini Monitor',
+    price: '145$',
+    description: 'The BlackMagic Design UltraStudio Mini Monitor is perfect for using low cost HDMI TVs and projectors for monitoring. When you need low cost monitoring for editing, then UltraStudio Mini Monitor is perfect. Plug in the Thunderbolt and monitor out of the SDI and HDMI outputs.',
+    rating: 4.5,
+    image1: 'https://images-na.ssl-images-amazon.com/images/I/71xLUryMUAL._AC_SL1181_.jpg',
+    image2: 'https://images-na.ssl-images-amazon.com/images/I/719ilFvhqbL._AC_SL1181_.jpg',
+    image3: 'https://images-na.ssl-images-amazon.com/images/I/41Hz1fncTeL._AC_.jpg',
+    link: 'https://www.amazon.com/Blackmagic-Design-UltraStudio-Playback-BDLKULSDZMINMON/dp/B009D912TM/ref=pd_di_sccai_4?pd_rd_w=V7OoD&pf_rd_p=c9443270-b914-4430-a90b-72e3e7e784e0&pf_rd_r=XW3501SB82GWD3JJJSGQ&pd_rd_r=b02a2b8b-8fb9-40aa-b7fb-2b0f6ab1880e&pd_rd_wg=B5eMm&pd_rd_i=B009D912TM&psc=1'
+  },
+  {
+    id: 9,
+    name: 'Guermok 4K 60FPS Video Capture Card',
+    price: '39.99$',
+    description: 'Loop-out HD Game Capture Card: This video capture card is equipped with 5 ports: Type-C port, HD In& Out, Mic in and Audio out. This video capture device is based on the USB3.0 high-speed transmission port, support Input Max Resolution 4K/ 30HZ , Output Max Resolution 1080P/ 60HZ, support 3.5 stereo channel output',
+    rating: 3.5,
+    image1: 'https://images-na.ssl-images-amazon.com/images/I/61AxfccSRXL._AC_SL1500_.jpg',
+    image2: 'https://images-na.ssl-images-amazon.com/images/I/71O9BoOJPBL._AC_SL1500_.jpg',
+    image3: 'https://images-na.ssl-images-amazon.com/images/I/71Jw5K1n2sL._AC_SL1500_.jpg',
+    link: 'https://www.amazon.com/dp/B08SLRS94D/ref=sspa_dk_detail_4?psc=1&pd_rd_i=B08SLRS94D&pd_rd_w=7bKVh&pf_rd_p=4269e1a0-a218-4fbd-9748-1cd337d2f2a5&pd_rd_wg=S0Sss&pf_rd_r=51S63SB1XN8CNX40001F&pd_rd_r=a6624eb8-74a6-4c89-b556-d3c4526bbe73&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUExUzA4UlNPOE5PQjlMJmVuY3J5cHRlZElkPUEwNTc0NzE4M043RDhMRVUwUjRENyZlbmNyeXB0ZWRBZElkPUEwMjQwNzgyMU1aUU9NVTM2SkkyOCZ3aWRnZXROYW1lPXNwX2RldGFpbCZhY3Rpb249Y2xpY2tSZWRpcmVjdCZkb05vdExvZ0NsaWNrPXRydWU='
+  },
+  {
+    id: 10,
+    name: 'MiraBox Capture Card,USB 3.0 HDMI Game Capture Card',
+    price: '79.99$',
+    description: 'Mirabox USB3.0 HDMI Game Capture Card HSV321 - System Requirements and Technical Specifications',
+    rating: 4,
+    image1: 'https://images-na.ssl-images-amazon.com/images/I/711Wn3p1AyL._AC_SL1500_.jpg',
+    image2: 'https://images-na.ssl-images-amazon.com/images/I/81DQjWedkgL._AC_SL1500_.jpg',
+    image3: 'https://images-na.ssl-images-amazon.com/images/I/71ccRN2QheL._AC_SL1500_.jpg',
+    link: 'https://www.amazon.com/MiraBox-Loop-out-Streaming-Recording-HSV321/dp/B07C6KCBYB?ref_=ast_sto_dp'
+  },
+];
+
